14d3cd20e0dd35983ed321de9ba493e124fef60c
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-04-20 16:20:28 -0500

    second subnet

--- subnet.tf
+++ subnet.tf
@@ -3,4 +3,21 @@ resource "aws_subnet" "dev1" {
   cidr_block = "10.0.1.0/24"
 
   
+  tags {
+    Name = "terraform-january-${var.Created_by}-${count.index +1}"
+    Env = "${var.Env}"
+    Dept = "${var.Dept}"
+    Created_by = "${var.Created_by}"
+
+
+resource "aws_subnet" "dev1" {
+  vpc_id     = "${aws_vpc.dev.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags {
+    Name = "terraform-january-${var.Created_by}-${count.index +1}"
+    Env = "${var.Env}"
+    Dept = "${var.Dept}"
+    Created_by = "${var.Created_by}"
   }
+}
