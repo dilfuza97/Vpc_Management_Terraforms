f96a9cbff44b71d2463c9778593199d209007c44
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2019-04-20 18:38:54 -0500

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance"  "web" {
    subnet_id = "${aws_subnet.dev1.id}" 
    associate_public_ip_address = "true"
    vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-   }
+   
 
    tags {
      Name = "terraform-january-${var.Created_by}-${count.index +1}"
@@ -14,3 +14,4 @@ resource "aws_instance"  "web" {
      Dept = "${var.Dept}"
      Created_by = "${var.Created_by}"
   }
+ }
