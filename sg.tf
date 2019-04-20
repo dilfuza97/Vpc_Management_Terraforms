37c796b7c5c7e6f3ae4a63977199d9185fc24e16
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-04-20 16:58:45 -0500

    fix block security group

--- sg.tf
+++ sg.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "allow_ssh" {
     to_port     = 22
     protocol    = "tcp"
     
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 22
