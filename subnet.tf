5be2f301ea2df1c8ed2864049de6d1e8ffd6adf7
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-04-20 16:29:57 -0500

    subnet

--- subnet.tf
+++ subnet.tf
@@ -12,9 +12,9 @@ resource "aws_subnet" "dev1" {
     Created_by = "${var.Created_by}"
 
 
-resource "aws_subnet" "dev1" {
+resource "aws_subnet" "dev2" {
   vpc_id     = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
 
   tags {
     Name = "terraform-january-${var.Created_by}-${count.index +1}"
