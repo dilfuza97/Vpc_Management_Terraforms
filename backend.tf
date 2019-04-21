904196e87261e1678e63d39837bcc8198d585e61
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-04-21 12:47:46 -0500

    change name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-     bucket = "terraform-state-january-dilfuza2"
+     bucket = "terraform-state-december-dilfuza2"
      key = "infra.state"
      region = "eu-west-1" 
   }
--- bucket.tf
+++ bucket.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "newbucket" {
   bucket = "terraform-january-dilfuza2"
 
   tags {
-     Name = "terraform-january-dilfuza2" 
+     Name = "terraform-december-dilfuza2" 
      Env  = "dev" 
      Dept = "IT" 
      Created_by = "dilfuzs"
