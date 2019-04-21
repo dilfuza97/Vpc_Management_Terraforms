d6f6358ee17e615796bb6ad7a8ef1597492855f6
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-04-21 11:23:22 -0500

    add

--- db.asg.tf
+++ db.asg.tf
@@ -5,18 +5,18 @@ module "db-asg" {
 
   # Launch configuration
   lc_name = "example-lc-db"
-  image_id        = "${var.ami}"
-  instance_type   = "${var.instance_type}"
+  image_id        = "${var.db_ami}"
+  instance_type   = "${var.db_instance_type}"
   security_groups = ["${aws_security_group.allow_ssh.id}"]
 
   # Auto scaling group
   asg_name                  = "example-asg"
   vpc_zone_identifier       = ["${aws_subnet.dev2.id}"]
   health_check_type         = "EC2"
-  min_size                  = "${var.min_size}"
-  max_size                  = "${var.max_size}"
-  desired_capacity          = "${var.desired_capacity}"
-  wait_for_capacity_timeout = 0
+  min_size                  = "${var.db.min_size}"
+  max_size                  = "${var.db.max_size}"
+  desired_capacity          = "${var.db.desired_capacity}"
+  wait_for_capacity_timeout =  0
   associate_public_ip_address	= "true"
 }
 
--- varible.tf
+++ varible.tf
@@ -5,11 +5,22 @@ variable "key_name" {}
 variable "Created_by" {}
 variable "Env" {}
 variable "Dept" {}
-variable "Appname" {
+variable "Appname" {}
+
+variable "min_size" {}
+variable "max_size" {}
+variable "desired_capacity" {}
+variable "DBname" {}
+
+variable "db_min_size" {}
+variable "db_max_size" {}
+variable "db_desired_capacity" {}
+
+variable "db_ami"  {}
+variable "db_instance_type" {}
+
+
   
-}
-
-
 
 
   
