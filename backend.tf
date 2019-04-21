e6e35b864271861252c6594ba07925cb28388d5e
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-04-21 10:48:35 -0500

    backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-     bucket = "terraform-state-january-dilfuza"
+     bucket = "terraform-state-january-dilfuza2"
      key = "infra.state"
      region = "eu-west-1" 
   }
