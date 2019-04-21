d8c108a84f206e3b3335ae882d056d3216985f00
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-04-21 11:08:55 -0500

    FIXED

--- db.asg.tf
+++ db.asg.tf
@@ -1,27 +1,23 @@
-module "asg" {
+module "db-asg" {
   source = "terraform-aws-modules/autoscaling/aws"
 
   name = "${var.DBname}"
 
   # Launch configuration
-  lc_name = "example-db"
-
+  lc_name = "example-lc-db"
   image_id        = "${var.ami}"
   instance_type   = "${var.instance_type}"
   security_groups = ["${aws_security_group.allow_ssh.id}"]
 
-  
-
   # Auto scaling group
   asg_name                  = "example-asg"
-  vpc_zone_identifier       = ["${aws_subnet.dev1.id}"]
+  vpc_zone_identifier       = ["${aws_subnet.dev2.id}"]
   health_check_type         = "EC2"
   min_size                  = "${var.min_size}"
   max_size                  = "${var.max_size}"
   desired_capacity          = "${var.desired_capacity}"
   wait_for_capacity_timeout = 0
-  wait_for_capacity_timeout = 0
-  associate_public_ip       = "true"   
-
+  associate_public_ip_address	= "true"
+}
 
 }
