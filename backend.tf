b56e582ca1454a0551fd8af13523fe3d7bdcb9be
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-07-31 18:19:41 -0500

    change

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-     bucket = "terraform-state-december-dilfuza2"
-     key = "infra.state"
-     region = "eu-west-1" 
+    bucket = "dilfuza-nagiosxi"
+    key    = "infra.state"
+    region = "us-east-1"
   }
 }
